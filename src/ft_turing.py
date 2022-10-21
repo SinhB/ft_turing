84aaead12dc928caa02c660caa2753f2c78e0252
Lou CHANCRIN <lchancri@e3r7p11.clusters.42paris.fr>
2022-10-21 17:27:32 +0200

    Bonus completed and wip: iterative engine

--- src/ft_turing.py
+++ src/ft_turing.py
@@ -3,12 +3,12 @@
 import argparse
 from json import JSONDecodeError, load as json_load
 import itertools
-import math
-import matplotlib.pyplot as plt
 
 from engine import engine
+from run_engine import run_engine
 from utils.error import ParsingError
 from utils.health_check import checks
+from utils.plot import plot_complexity
 
 def generate_strings(chars, len):
     return [''.join(x) for x in itertools.product(chars, repeat=len)]
@@ -19,79 +19,59 @@ def get_input():
     """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--jsonfile", dest="jsonfile", help="json description of the machine")
-    parser.add_argument("--input", dest="input", help="input of the machine", type=str)
-    parser.add_argument("--bonus", dest="bonus", action=argparse.BooleanOptionalAction, help="complexity bonus", type=bool, default=False)
+    parser.add_argument("jsonfile", dest="jsonfile", help="json description of the machine")
+    parser.add_argument("input", dest="input", help="input of the machine", type=str)
+    parser.add_argument("bonus", dest="bonus", action=argparse.BooleanOptionalAction, help="complexity bonus", type=bool, default=False)
     return parser.parse_args()
 
-def plot_complexity(machine_result):
-    linear_n = [0]
-    O_logn = [0]
-    O_n_logn = [0]
-    O_n2 = [0]
-    O_2n = [0]
-    O_factorialn = [0]
-    O_n_sqrtn = [0]
-    O_sqrtn = [0]
-    for n in range(1, 100):
-        linear_n.append(n)
-        O_logn.append(math.log(n))
-        O_n_logn.append(n * math.log(n))
-        O_n2.append(n ** 2)
-        O_2n.append(2 ** n)
-        O_factorialn.append(math.factorial(n))
-        O_n_sqrtn.append(n * math.sqrt(n))
-        O_sqrtn.append(math.sqrt(n))
-    plt.plot(O_logn, color='red')
-    plt.plot(linear_n, color='green')
-    plt.plot(O_n_logn, color="purple")
-    plt.plot(O_n2, color='cyan')
-    plt.plot(O_2n, color='orange')
-    plt.plot(O_factorialn, color='lightblue')
-    plt.plot(O_n_sqrtn, color='yellow')
-    plt.plot(O_sqrtn, color='magenta')
-
-    plt.plot(machine_result, color='black')
-    plt.legend(['O(logn)', 'O(n)', 'O(nlogn)', 'O(n**2)', 'O(2**n)', 'O(n!)', 'O(n_sqrtn)', 'O(sqrtn)', 'O(machine)'])
-    plt.xlim([0, 100])
-    plt.ylim([0, 1000])
-    plt.grid()
-    plt.show()
-
-if __name__ == "__main__":
-    args = get_input()
-    plot_complexity([0, 3, 6, 9, 14, 19, 26, 33, 42, 51])
-    import sys
-    sys.exit()
-    if args.bonus:
-        with open(args.jsonfile, encoding="utf-8") as machine_description:
+def bonus():
+    files_path = {
+        '0n1n': 'machines_json/0n1n.json',
+        '02n': 'machines_json/02n.json',
+        'palindrome': 'machines_json/palindrome.json',
+        'unary_add': 'machines_json/unary_add.json'
+    }
+    max_complexity = {'0n1n': [0], '02n': [0], 'palindrome': [0], 'unary_add': [0]}
+    for key in files_path:
+        with open(files_path[key], encoding="utf-8") as machine_description:
             machine = json_load(machine_description)
-        max_complexity = [0]
-        for i in range(1, 10):
+        for i in range(1, 4):
             no_blank_alphabet = [x for x in machine['alphabet'] if x not in machine['blank']]
             inputs = generate_strings(no_blank_alphabet, i)
             complexity = []
             for single_input in inputs:
-                # print(single_input)
                 try:
                     complexity.append(engine(
                         machine=machine,
                         current_state=machine["initial"],
                         tape=(single_input + "."),
-                        # tape=input,
                         head=0,
-                        count=0
+                        count=0,
+                        silent=True
                     ))
                 except:
                     pass
             if complexity:
-                max_complexity.append(max(complexity))
+                max_complexity[key].append(max(complexity))
             else:
-                max_complexity.append(0)
-        print(max_complexity)
+                max_complexity[key].append(0)
+    plot_complexity(max_complexity)
+
+if __name__ == "__main__":
+    args = get_input()
+    if args.bonus:
+        bonus()
     else:
         try:
             machine, input = checks(args)
+            # x = {
+            #     'machine': machine,
+            #     'current_state': machine["initial"],
+            #     'tape': (input + "."),
+            #     'head': 0,
+            #     'count': 0
+            # }
+            # run_engine(x)
             engine(
                 machine=machine,
                 current_state=machine["initial"],
--- src/run_engine.py
+++ src/run_engine.py
@@ -1,7 +1,6 @@
 import sys
 from termcolor import colored
-
-sys.setrecursionlimit(10000)
+from utils.error import NoTransitionError, NoSolutionError
 
 def update_tape(tape: str, head: str, write: str):
     if tape[-1] != '.':
@@ -18,12 +17,6 @@ def print_tape(tape:str, head: str, current_state: str, transition: object):
         new_tape = new_tape.replace("Q", colored("Q", "blue"))
     if tape[head] != 'Z':
         new_tape = new_tape.replace("Z", colored("Z", "red"))
-    print(
-        f"[{new_tape}]"
-        f"  |  ({current_state}, {tape[head]}) ->"
-        f" ({transition[0]['to_state']},"
-        f" {transition[0]['write']}, {transition[0]['action']})"
-    )
 
 def get_action(head, action):
     if action == "RIGHT":
@@ -38,43 +31,24 @@ def perform_transition(
     head: int,
     current_state: str,
     transition: object,
-    count: int,
-    silent: bool
+    count: int
 ):
     if len(transition) != 1:
-        raise SyntaxError(f"Expected 1 transition for state {current_state}" + f" with read {tape[head]}, got {len(transition)}")
-        # sys.exit(
-        #     f"Expected 1 transition for state {current_state}"
-        #     f" with read {tape[head]}, got {len(transition)}"
-        # )
-    if not silent:
-        print_tape(tape, head, current_state, transition)
-    return engine(
-        machine,
-        transition[0]["to_state"],
-        update_tape(tape, head, transition[0]["write"]),
-        get_action(head, transition[0]["action"]),
-        count + 1,
-        silent
-    )
-    # engine(
-    #     machine,
-    #     transition[0]["to_state"],
-    #     update_tape(tape, head, transition[0]["write"]),
-    #     head + 1 if transition[0]["action"] == "RIGHT" else head - 1,
-    # )
+        raise NoTransitionError(f"Expected 1 transition for state {current_state}" + f" with read {tape[head]}, got {len(transition)}")
+    print_tape(tape, head, current_state, transition)
+    return {
+        "machine": machine,
+        "current_state": transition[0]["to_state"],
+        "tape": update_tape(tape, head, transition[0]["write"]),
+        "head": get_action(head, transition[0]["action"]),
+        "count": count + 1
+    }
 
-
-def engine(machine: object, current_state: str, tape: list, head: int, count: int, silent: bool = False):
+def engine(machine: object, current_state: str, tape: list, head: int, count: int):
     if current_state in machine["finals"]:
         return count
-        # sys.exit(
-        #     # f"[{tape[0:head]}<{tape[head]}>{tape[head+1:]}]"
-        #     # f"  |  ({current_state}, {tape[head]})"
-        # )
     if head == len(tape) or head < 0:
-        raise SyntaxError("no solution")
-        # sys.exit("no solution")
+        raise NoSolutionError("no solution")
     return perform_transition(
         machine,
         tape,
@@ -86,6 +60,16 @@ def engine(machine: object, current_state: str, tape: list, head: int, count: in
                 machine["transitions"][current_state],
             )
         ),
-        count,
-        silent
+        count
     )
+
+def run_engine(x: dict):
+    while (type(x) != int):
+        x = engine(
+            x['machine'],
+            x['current_state'],
+            x['tape'],
+            x['head'],
+            x['count']
+        )
+    return x
